3d98e8621ce5884b85764f0d284697721af21ab0
seanchoii <99775502+seanchoii@users.noreply.github.com>
2023-12-22 02:24:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,36 @@
+
+
+var bubbleProfile = document.getElementById("bubbleProfile");
+var bubbleContact = document.getElementById("bubbleContact");
+var pixelAdventure = document.getElementById("bubblePixelAdventure");
+var portfolio = document.getElementById("mywebsite");
+var tottenham = document.getElementById("tottenhamP");
+var seansational = document.getElementById('seansational');
+var pig = document.getElementById('pig');
+const listProjects = [pixelAdventure, portfolio, tottenham, seansational, pig];
+const listProfile = [bubbleProfile, bubbleContact];
+
+var prev_handler = window.onload;
 window.onload = function() {
     var date = new Date();
     document.getElementById("time").innerHTML = date.toLocaleDateString("en-US");
-  }
-  
-var prev_handler = window.onload;
-
-var profileButton = document.getElementById("profile");
-var contactButton = document.getElementById("contact");
+    };
 
 
-function toggleProfile(){
-    var bubbleProfile = document.getElementById("bubbleProfile");
-    var bubbleContact = document.getElementById("bubbleContact");
-
-    if(bubbleProfile.style.display == "none")
-    {
-        bubbleProfile.style.display = "block";
-        bubbleContact.style.display = "none";
-    }else
-    {
-        bubbleProfile.style.display = "none";
-    }
-
+function toggleUser(num){
+    if(listProfile[num].style.display === "block"){
+        listProfile[num].style.display = "none";
+    }else{
+        listProfile[0].style.display = "none";
+        listProfile[1].style.display = "none";
+        listProfile[num].style.display = "block";
+    }    
 }
 
-function toggleContacts(){
-    var bubbleContact = document.getElementById("bubbleContact");
-    var bubbleProfile = document.getElementById("bubbleProfile");
-
-    if(bubbleContact.style.display == "none")
-    {
-        bubbleContact.style.display = "block";
-        bubbleProfile.style.display = "none";
-    }else
-    {
-        bubbleContact.style.display = "none";
+function toggleProfile(num){
+    for(i = 0; i < listProjects.length; i++){
+        listProjects[i].style.display = "none";
     }
+    listProjects[num].style.display = "block";
 }
-
-function togglePixelAdventure(){
-    var pixelAdventure = document.getElementById("bubblePixelAdventure");
-    var portfolio = document.getElementById("mywebsite");
-    var tottenham = document.getElementById("tottenhamP");
-
-    if(pixelAdventure.style.display == "none")
-    {
-        pixelAdventure.style.display = "block";
-        portfolio.style.display = "none";
-        tottenham.style.display = "none";
-
-    }else
-    {
-        pixelAdventure.style.display = "none";
-    }
-}
-
-function togglePortfolio(){
-    var pixelAdventure = document.getElementById("bubblePixelAdventure");
-    var portfolio = document.getElementById("mywebsite");
-    var tottenham = document.getElementById("tottenhamP");
-
-    if(portfolio.style.display == "none")
-    {
-        pixelAdventure.style.display = "none";
-        portfolio.style.display = "block";
-        tottenham.style.display = "none";
-
-    }else
-    {
-        portfolio.style.display = "none";
-    }
-}
-
-function toggleTottenham(){
-    var pixelAdventure = document.getElementById("bubblePixelAdventure");
-    var portfolio = document.getElementById("mywebsite");
-    var tottenham = document.getElementById("tottenhamP");
-
-    if(tottenham.style.display == "none")
-    {
-        pixelAdventure.style.display = "none";
-        portfolio.style.display = "none";
-        tottenham.style.display = "block";
-
-    }else
-    {
-        tottenham.style.display = "none";
-    }
-}
-
 
